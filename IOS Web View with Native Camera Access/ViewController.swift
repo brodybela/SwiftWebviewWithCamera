3da4e61cee95e2e85ddfce1a403ddc6b9a037999
Bródy Béla <brody@mbbela.local>
2017-02-04 18:13:18 +0100

    Initial Commit

---
+++ IOS Web View with Native Camera Access/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IOS Web View with Native Camera Access
+//
+//  Created by Bela Brody on 04/02/2017.
+//  Copyright © 2017 Brody Media. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
